846b18107ebcba61f214d0dc2d11649a52db8564
Amanda Braga <amandabraga1998@gmail.com>
2026-01-12 12:01:33 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 function Home() {
-    return <h1>Obrigado por estar do meu lado vida! Eu te amo :)</h1>
-    
+  return <h1>Obrigado por estar do meu lado vida! Eu te amo :)</h1>;
 }
 
-export default  Home;
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
+}
+
+export default Home;
