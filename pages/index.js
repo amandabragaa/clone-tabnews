f9239c5d1b3bfac6e98c45e10f25d799e5f4edb2
Amanda Braga <amandabraga1998@gmail.com>
2026-01-16 11:13:22 -0300

    install `jest` and configure test scripts

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Obrigado por estar do meu lado vida! Eu te amo :)</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home;
